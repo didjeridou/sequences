eeae946092b8a5358ae52e94b1012856498a9feb
Will <glaliberte@g.harvard.edu>
2015-04-29 21:51:05 -0400

    Cleaned-up commands, added comments for documentation

--- sequences.ml
+++ sequences.ml
@@ -11,19 +11,21 @@
  * http://en.wikipedia.org/wiki/FASTA_format *)
 (* ftp://ftp.ncbi.nih.gov/genomes/Viruses/ *)
 
-(** example.ml: The first special comment of the file is the comment
-    associated with the whole module. *)
-
+(* ABOUT MODULES:
+ * DNASequence is the module for, you guessed it, a
+ * DNA sequence. It is built over a string suffix array
+ * 
+ * Tools is a custom module that isolates useful functions
+ * like printing the header of the program at launch or 
+ * showing the custom console for executing commands. *)
 open Core.Std
 open DNASequence
-open Meta
+open Tools
 
-(* Just a small pattern to make quick tests *)
-let data_test_pattern = "e";;
-let opened_cfna = ref "No opened CFNA file";;
-
-(* We create our module DNA to use strings as prefixes since
- * DNA data is in fact a series of A,T,C and G *)
+(* Module DNA to manipulate strings of DNA as suffix since
+ * DNA data is in fact a series of A,T,C and G.
+ * See the full implementation (as string suffix arrays)
+ * in DNASequence.ml *)
 module DNA = DNASequence.Make(
   struct
     type t = string
@@ -36,80 +38,85 @@ module DNA = DNASequence.Make(
     let string_of_t t = t
   end)
 
-let string_of_result (str: string) (dna: DNA.seq) : string = 
-  match DNA.pattern_search str dna with
-  | None -> "\n Not Found \n"
-  | Some (_, i) -> 
-    ("\nFound Your Pattern at Position " ^ string_of_int i)
-(*    ("\nFound " ^ string_of_int i ^ ": " ^ DNA.string_of_subseq s) *)
+(* ABOUT .cfna FILES:
+ * .fna files are for the FASTA Format, a special format fo
+ * ATCG sequences. All the data used in this demo was obtained
+ * as .fna files from ftp://ftp.ncbi.nih.gov/genomes/
+ *
+ * We use a custom format, .cfna or condensed .fna which is
+ * basically the fna file without the newline characters.
+ *
+ * CONVENTION: 
+ * All reference names are preceded by '_' for
+ * a better readability / quick identification
+ *
+ * _data: ref to an empty DNA seq. Will hold our final DNA.seq
+ * _cfna: ref to a string containing the CFNA meta data.
+ * _subcommands: ref to what will be our 'controls' for the program.
+ * It is a Command group for the shell. *)
+
+let _data = ref DNA.empty;;
+let _cfna = ref "No opened CFNA file";;
+let _subcommands =
+  ref (Command.group 
+        ~summary: ("")
+        [])
 ;;
 
-(* Open a .dna file and make it a string for use with our data struct *)
+(* HELPER: parses a .cfna file. 
+ * Get the metadata, sets the _cfna metadata and get the string 
+ * of ATCG to create the DNA.seq *)
 let parse_cfna file =
   In_channel.with_file file ~f:(fun ic ->
-    ignore(opened_cfna := input_line ic);
-    input_line ic
-  )
+    _cfna := input_line ic;
+    Tools.current_seq !_cfna;
+    let atcg = input_line ic in 
+      Some atcg)
 
-(* Helper function to index a CFNA into our DNA data structure *)
-let dna_from_cfna file : DNA.seq option = 
-  ignore(print_string "\n\n[STATUS] Indexing DNA...\n\n";);
-  Some (DNA.from_string (parse_cfna file))
+(* HELPER: index the ATCG data from the CFNA into our DNA
+ * data structure (built over a string suffix array) and
+ * set _data to our new data *)
+let load_cfna file = 
+  let parsed = parse_cfna file in
+    match parsed with
+    | None -> print_string "No ATCG in file in selected file"
+    | Some loaded -> 
+      print_string "\n\n[STATUS] Indexing DNA...\n\n";
+      _data := (DNA.from_string loaded);
 ;;
 
-let rec open_to_analyze (data: DNA.seq option) =
-  Meta.current_seq !opened_cfna;
-  Meta.avail_commands ();
+(* Main program where we show the console to the user.
+ * Using subcommands, the user will be able to control
+ * the program and perform operations on the DNA (_data) *)
 
-  Meta.console ();
-
-  let list_data =
-    Command.basic 
-      ~summary:"List the CFNA data in ./_data"
-      Command.Spec.(
-        empty
-        +> anon ("test" %: string)
-      )
-      (fun dna_seq () -> 
-        print_string "Success"
-        (*Meta.list_data ();
-                open_to_analyze data*))
-  in
-
-  let exit_program =
-    Command.basic 
-      ~summary:"Exit Sequences"
-      Command.Spec.(
-        empty
-      )
-      (fun () -> Meta.exit_program ())
-  in
-
-  let controls =
-    Command.group 
-      ~summary: ("")
-      [ "data", list_data; "exit", exit_program ]
-  in
+let rec main () =
+  Tools.console ();
 
   match In_channel.input_line stdin with
-  | None -> open_to_analyze data
-  | Some cmd -> 
+  | None -> main ()
+  | Some cmd ->
     Command.run 
-      ~argv: (String.split cmd ~on:' ')
-      controls
-
-(*   match In_channel.input_line stdin with
-  | None -> open_to_analyze data
-  | Some cmd -> print_string cmd
- *)
-(*   match data with
-  | None -> Meta.empty_data ()
-  | Some d ->
-    print_string (
-      string_of_result data_test_pattern d) *)
+      ~argv: ("unrecognized"::(String.split cmd ~on:' '))
+      !_subcommands
 ;;
 
-let use_cfna =
+(* COMMANDS & SUBCOMMANDS
+ * Here are all the available commands and subcommands.
+ * Commands are arguments that can be passed to 
+ * ./sequences.native while subcommands can be used once
+ * Sequences is launched. You will see 
+ *    [Sequences] >
+ * when the program runs. There you can run subcommands*)
+
+
+(*###########
+ * COMMANDS
+ *##########*)
+
+(* Load the base file in the memory
+ * This loads the file and creates a new DNA.seq, itself built
+ * over a suffix array. *)
+let load_cfna_cmd =
   Command.basic 
     ~summary:"Indexes a base DNA (.cfna) file to perform operations"
     Command.Spec.(
@@ -117,26 +124,72 @@ let use_cfna =
       +> anon ("filename" %: file)
     )
     (fun filename () -> 
-      Meta.heading ();
-      open_to_analyze (dna_from_cfna filename))
+      Tools.heading ();
+      load_cfna filename;
+      print_string 
+        "[STATUS] Indexing complete
+        \n[INFO] Perfom analysis using the following commands.\n";
+      Tools.avail_commands ();
+      main ())
+;;
 
-let default =
-  Command.basic 
-    ~summary:"Open Sequences without a file"
+(* Associating textual commands to actions *)
+let command =
+  Command.group ~summary: (Tools.get_intro () ^ Tools.get_summary ())
+    [ "load", load_cfna_cmd ]
+;;
+
+(*##############
+ * SUBCOMMANDS
+ *#############*)
+
+(* Subcommand 'exit' *)
+let exit_program =
+  Command.basic ~summary:"Exit Sequences" Command.Spec.(empty)
+    (fun () -> Tools.exit_program ())
+;;
+
+(* Subcommand 'data', lists the files available in our data
+ * folder, ./_data *)
+let list_data =
+  Command.basic ~summary:"List the CFNA data in ./_data"
+    Command.Spec.(empty)
+    (fun () -> 
+      Tools.list_data ();
+      main ())
+;;
+
+(* Subcommand 'search STRING'. Searches for a string in our indexed
+ * DNA sequence and returns the position. *)
+let search =
+  Command.basic ~summary:"List the CFNA data in ./_data"
     Command.Spec.(
       empty
+      +> anon ("str" %: string)
     )
-    (fun () -> 
-      (Meta.heading ());
-      open_to_analyze None)
+    (fun str () -> 
+      let res = DNA.pattern_search str !_data in 
+        match res with
+        | None -> Tools.not_found (); main ();
+        | Some (_, i) -> Tools.found_at i; main (); )
+;;
 
-let command =
-  Command.group 
-    ~summary: (Meta.get_intro () ^ Meta.get_summary ())
-    [ "use", use_cfna ]
+(* To prevent empty subcommands to end the program *)
+let return =
+  Command.basic ~summary:"" Command.Spec.(empty) (fun () -> main ())
+;;
 
-let () = Command.run 
-          ~version: (Meta.get_version ()) 
-          ~build_info:"DNA" 
-          command
+_subcommands :=
+    Command.group ~summary: ("")
+      [ 
+        "data", list_data; 
+        "exit", exit_program; 
+        "", return;
+        "search", search
+      ]
+;;
+
+(* Finally, we run Command.run *)
+let () = Command.run ~version: (Tools.get_version ()) 
+          ~build_info:"DNA" command
 
